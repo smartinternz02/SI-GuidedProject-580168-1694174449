d99e7a9e22c0852e17e79ec21be879440c4ac483
G Jyotish kumarr <139673718+JyotishKumarr@users.noreply.github.com>
2023-09-13 15:50:22 +0530

    Update MainActivity.kt

--- SmartBridgeAssignment3/MainActivity.kt
+++ SmartBridgeAssignment3/MainActivity.kt
@@ -1,1 +1,150 @@
+package com.example.smartbridgeassignment3
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.smartbridgeassignment3.ui.theme.SmartBridgeAssignment3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyTheme()
+        }
+    }
+}
+
+@Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTheme(){
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val radioOptions = listOf("https://www.google.com/", "https://www.youtube.com/", "https://www.amazon.in/")
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[2]) }
+
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Login Image",
+            modifier = Modifier
+                .size(150.dp)
+                .padding(16.dp)
+        )
+        Text(
+            text = "Login",
+            style = MaterialTheme.typography.headlineMedium.copy(
+                color = Color(0xFF476810)
+            )
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = username, onValueChange = { username = it },
+            label = { Text(text = "Username") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = password, onValueChange = { password = it },
+            label = { Text(text = "Password") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            )
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        radioOptions.forEach { text ->
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .selectable(
+                        selected = (text == selectedOption),
+                        onClick = { onOptionSelected(text) }
+                    )
+                    .padding(horizontal = 16.dp)
+            ) {
+                RadioButton(
+                    selected = (text == selectedOption),
+                    modifier = Modifier.padding(all = Dp(value = 8F)),
+                    onClick = {
+                        onOptionSelected(text)
+                        Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+                    }
+                )
+                Text(
+                    text = text,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(selectedOption)
+            /*
+            if (intent.resolveActivity(context.packageManager)!=null){
+                context.startActivity(intent)
+            }else{
+                Toast.makeText(context,"No web browser available",Toast.LENGTH_LONG).show()
+            }*/
+
+            try {
+                context.startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(context, "No web browser available", Toast.LENGTH_LONG).show()
+            }
+        }, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Login")
+        }
+    }
+}
 
